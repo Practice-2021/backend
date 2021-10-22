535524701876928cf159077037aac8357500b3d6
avtimofeev <al.timofeev.m@yandex.ru>
2021-10-22 23:44:50 +0300

    Добавил конфигурацию для Mongodb

---
+++ src/main/java/edu/citik/configuration/MongoConfig.java
@@ -0,0 +1,35 @@
+package edu.citik.configuration;
+
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
+
+@Configuration
+public class MongoConfig extends AbstractMongoClientConfiguration {
+
+    @Value("${spring.data.mongodb.database}")
+    String databaseName;
+
+    @Value("${spring.data.mongodb.url}")
+    String url;
+
+
+    @Override
+    protected String getDatabaseName() {
+        return databaseName;
+    }
+
+    @Override
+    public MongoClient mongoClient() {
+        ConnectionString connectionString = new ConnectionString(url);
+        MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
+                .applyConnectionString(connectionString)
+                .build();
+
+        return MongoClients.create(mongoClientSettings);
+    }
+}
